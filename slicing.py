58ef9269a4d76a13e9443b6b9897af6271a5caca
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-14 23:46:53 +0530

    Update slicing.py

--- slicing.py
+++ slicing.py
@@ -5,26 +5,26 @@
 
 #indexing
 
-#place = "pol kuvemppapa"
-#city = place[0:6]
-#area = place[7:19]
-#city = place[:6]
-#area = place[7:]
+place = "pol kuvemppapa"
+city = place[0:6]
+area = place[7:19]
+city = place[:6]
+area = place[7:]
 #step
-#punkcity = place[0:19:3]
-#punkcity = place[::3]
-#print(area)
-#print(city)
-#print(punkcity)
+punkcity = place[0:19:3]
+punkcity = place[::3]
+print(area)
+print(city)
+print(punkcity)
 
 #***how to reverse a string in python using slicing***
-#reversed_place = place[::-1]
-#print(reversed_place)
+reversed_place = place[::-1]
+print(reversed_place)
 
 #slice function part 2
 
-#website1 = "http://youtube.com"
-#website2 = "http;//twitch.com"
-#slice = slice(7,-4)
-#print(website1[slice])
-#print(website2[slice])
+website1 = "http://youtube.com"
+website2 = "http;//twitch.com"
+slice = slice(7,-4)
+print(website1[slice])
+print(website2[slice])
